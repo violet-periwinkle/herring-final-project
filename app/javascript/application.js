0111f8fc63aa5482befefac06b299098ecb5bd47
violet-periwinkle <134008799+violet-periwinkle@users.noreply.github.com>
2024-10-16 19:27:16 -0400

    began styling with bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,28 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "popper"
 import "bootstrap"
+// function findInkwell(target) {
+//     let loopCheck = 0;
+//     while (!target.parentElement.classList.contains("inkwell")) {
+//         target = target.parentElement;
+//         if (loopCheck > 6 ){
+//             console.log(target);
+//             break;
+//         }
+//     }
+//     return target.parentElement;
+// }
+// function buttonSetup(){
+//     removeColorButtons = Array.from(document.getElementsByClassName("delete-color-button"));
+// }
+// let removeColorButtons;
+// buttonSetup();
+// const addColorButton = document.getElementById("add-color-button");
+// addColorButton.addEventListener('click', buttonSetup);
+// removeColorButtons.forEach(btn => {
+//     btn.addEventListener('click', (e) => {
+//         const colorControl = findInkwell(e.target);
+//         colorControl.style.display = "none";
+//     })
+// });
+// })
